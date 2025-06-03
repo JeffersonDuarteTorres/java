8dbbdb0ba82afc07320c7c6add4bbd56bd2e1542
JeffersonDuarteTorres <jeffersonduartetete@gmail.com>
2025-06-03 16:49:39 -0500

    3/06/2025 Main y grafica

--- InterfazGrafica/src/Grafica.java
+++ InterfazGrafica/src/Grafica.java
@@ -10,6 +10,10 @@ public class Grafica extends JFrame {
     
     public Grafica(){
         this.setSize(400,500);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("Bienvenido a www.sena.com.ed.co");
+        this.setLocation(550, 200);
+        this.setBounds(550,200,400,500);
     }
     
 }
